968af5a6e114621fea3cf5c4bf127fe98cf3fb49
code-readers5000 <dean27gregory@gmail.com>
2022-08-11 20:50:40 +0200

    frtnfred

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -4,19 +4,20 @@ const chain = 'polygon'; // rinkeby or polygon
 const welcome_h1 = "Welcome to BasedWhales!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = 'BasedWhales is a community-driven collection of 5,000 whales thriving on the Polygon Blockchain, each with unique features. The BasedWhales are confidently authentic, friendly, optimistic and always on the search for the next 100x. Our aim is to unite the crypto community, and inspire a sense of joyous amusement to see us through the bear market. Established or aspiring whales, everyone is welcome! We will come out the other side stronger, together, and BASED AF.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = " Minting soon, stay tuned!";
+const h1_presale_coming_soon = "Minting soon, stay tuned!";
+
 const h1_public_mint = "Public Minting Open!";
-const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
+const h2_presale_coming_soon = "PUBLIC MINT COUNTDOWN";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
+const p_presale_coming_soon = "";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "";
 const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
-const button_presale_coming_soon = "Get on the Whitelist";
+const button_presale_coming_soon = "join the community";
 const button_presale_mint_whitelisted = "Mint Your Special NFT";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
 const button_public_mint = "Mint Your NFT";
 const mint_failed = "Minting failed. 😢 Please try again.";
+
